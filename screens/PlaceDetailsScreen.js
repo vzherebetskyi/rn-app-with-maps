a6f15e80abf3a94bbbb0f6425f75b8f8430b89f6
vzherebetskyi <vzherebetskyi@gmail.com>
2021-08-24 10:23:29 +0300

    outputting list of places

--- screens/PlaceDetailsScreen.js
+++ screens/PlaceDetailsScreen.js
@@ -9,6 +9,12 @@ const PlaceDetailsScreen = props => {
   );
 };
 
+PlaceDetailsScreen.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('placeTitle'),
+  };
+};
+
 const styles = StyleSheet.create({});
   
 export default PlaceDetailsScreen;
